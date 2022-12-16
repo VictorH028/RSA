6c3dab8fd91a92c1e3c67cf3b30f2462211b8391
VictorH028 <demonhunternauta@gmail.com>
2022-12-16 10:40:15 -0500

    0.00

---
+++ RSA.cpp
@@ -0,0 +1,25 @@
+/*
+ * Algoritmos
+ Empezar
+
+1. Elige dos números primos p y q.
+ 2. Calcule n = p*q.
+ 3. Calcular phi = (p-1) * (q-1).
+ 4. Elija un número entero e tal que 1 < e < phi(n) y mcd(e, phi(n)) = 1;  es decir, e y phi(n) son coprimos.
+ 5. Calcular d como d ≡ e−1 (mod phi(n));  aquí, d es el inverso multiplicativo modular de e módulo phi(n).
+ 6. Para el cifrado, c = me mod n, donde m = mensaje original.
+ 7. Para el descifrado, m = c d mod n.
+
+Final
+*/
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+class RSA{ 
+	int p , q;
+   public:
+	
+};
